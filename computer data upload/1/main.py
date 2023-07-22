68a43e9448578cfd22fcf6618ebba22afd376458
Hedgr <74436143+Hedgr@users.noreply.github.com>
2023-07-22 01:46:39 -0700

    one time pad

--- computer data upload/1/main.py
+++ computer data upload/1/main.py
@@ -1,49 +1,63 @@
-import os
-import tempfile
-from qiskit.exceptions import MissingOptionalLibraryError
-from qiskit.circuit.library.phase_oracle import PhaseOracle
-from qiskit.algorithms import AmplificationProblem
-from qiskit.algorithms import Grover
-from qiskit.primitives import Sampler
-from qiskit.tools.visualization import plot_histogram
+import random
+
+true = True
+false = False
+debug = false
+
+in_str = ""
+out_str = ""
+
+charstring = "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?"
+keyList = []
 
 
-def do_grovers(num):
-    input_3sat_instance = '''
-    c example DIMACS-CNF 3-SAT
-    p cnf 3 5
-    -1 -2 -3 0
-    1 -2 3 0
-    1 2 -3 0
-    1 -2 -3 0
-    -1 2 3 0
-    '''
+# above is 94 chars long
 
-    fp = tempfile.NamedTemporaryFile(mode='w+t', delete=False)
-    fp.write(input_3sat_instance)
-    file_name = fp.name
-    fp.close()
-    oracle = None
-    try:
-        oracle = PhaseOracle.from_dimacs_file(file_name)
-    except ImportError as ex:
-        print(ex)
-    finally:
-        os.remove(file_name)
-
-    problem = None
-    if oracle is not None:
-        problem = AmplificationProblem(oracle, is_good_state=oracle.evaluate_bitstring)
-
-    grover = Grover(sampler=Sampler())
-    result = None
-    if problem is not None:
-        result = grover.amplify(problem)
-        print(result.assignment)
-
-    if result is not None:
-        plot_histogram(result.circuit_results[0], filename="histogram.png")
+# assumption is that key transfer is 100% secure
 
 
-# for i in range(2^15):
-byte = bytearray
+def getCharIndex(character):
+    for i in range(len(charstring)):
+        if character == charstring[i]:
+            return i
+    print("somethings wrong with getCharIndex()")
+
+
+def shuffleCharIndex():
+    pass
+
+
+def encrypt(string_in):
+    out = []
+    for i in range(len(string_in)):
+        randByte = random.getrandbits(8)
+        keyList.append(randByte)
+        to_ret = getCharIndex(string_in[i]) ^ randByte
+        out.append(to_ret)
+    return out
+
+
+def decrypt(string_in):
+    out = ""
+    for i in range(len(string_in)):
+        key = keyList[i]
+        toDecrypt = string_in[i]
+        toRet = toDecrypt ^ key
+        out = out + str(charstring[toRet])
+    return out
+
+
+def report(string):
+    to_encrypt = str(string)
+    encrypted = encrypt(to_encrypt)
+    decrypted = decrypt(encrypted)
+
+    print("start txt: " + str(to_encrypt))
+    print("encrypted: " + str(encrypted))
+    print("keys list: " + str(keyList))
+    print("decrypted: " + str(decrypted))
+
+
+while true:
+    keyList = []
+    report(input("input to be encrypted:\n"))
